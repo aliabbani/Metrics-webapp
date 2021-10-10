3388a44eb7921e0e4032b236cdffee5e8e35d810
ali abbani <aliox@ahlam.com>
2021-10-10 17:23:57 +0300

    Add AppContainer file

---
+++ src/components/AppContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppContainer = () => (
+  <div>
+    Hello from the app container
+  </div>
+);
+
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AppContainer from './components/AppContainer';
 import './stylesheet/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <div>Hello from the store</div>
+      <AppContainer />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
