47d752d34896e9d654e0ff7b02091c8cf1ec0087
ali abbani <aliox@ahlam.com>
2021-10-11 14:43:01 +0300

    Add store.js in the redux folder

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import dataReducer from './reducers/dataReducer';
+
+const reducer = combineReducers({
+  dataReducer,
+});
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
